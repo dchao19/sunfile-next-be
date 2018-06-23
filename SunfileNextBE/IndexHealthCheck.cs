a75e3439d230cfd8f349198c4f91d34d277f2204
Daniel Chao <daniel@danielchao.me>
2018-06-23 10:01:19 -0600

    Add boilerplate project structure

---
+++ SunfileNextBE/IndexHealthCheck.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SunfileNextBE
+{
+    public static class IndexHealthCheck
+    {
+        [FunctionName("IndexHealthCheck")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "ping")]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            return req.CreateResponse(HttpStatusCode.OK, "pong");
+        }
+    }
+}
